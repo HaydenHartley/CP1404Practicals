f689767501788b8b6dd89c5f404c8319cd09afd4
Hayden Hartley <hhart30@hotmail.com>
2017-03-20 07:33:45 +1000

    work on prac questions

--- Prac03/oddName.py
+++ Prac03/oddName.py
@@ -1,5 +1,14 @@
-name_input = input("enter your name: ")
-while len(name_input) < 1:
+def main():
+    name_input, name_length = get_name()
+    print (name_input[1:name_length:2])
+
+
+def get_name():
     name_input = input("enter your name: ")
-name_length = len(name_input)
-print (name_input[1:name_length:2])
+    while len(name_input) < 1:
+        name_input = input("enter your name: ")
+    name_length = len(name_input)
+    return name_input, name_length
+
+
+main()
