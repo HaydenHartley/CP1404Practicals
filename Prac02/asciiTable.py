ad5149ff390141ccf705b4c4c5dde1ba5ebeefee
Hayden Hartley <jc428741@ad.jcu.edu.au>
2017-03-19 19:41:07 +1000

    work on prac questions

--- Prac02/asciiTable.py
+++ Prac02/asciiTable.py
@@ -1,8 +1,18 @@
-character_input = raw_input("Enter a character: ")
-print("The ASCII code for {} is {}".format(character_input, ord(character_input)))
+LOWER_VALUE = 33
+UPPER_VALUE = 127
 
-#number_input = int(input("Enter a between 33 and 127: "))
-#while 33 > number_input & number_input > 127:
-#    number_input = int(input("Invalid number. Enter a between 33 and 127: "))
-#else:
-#    print("The character for {} is {}".format(character_input, chr(character_input)))
+def main():
+
+    character_input = input("Enter a character: ")
+    print("The ASCII code for {} is {}".format(character_input, ord(character_input)))
+
+    number_input = int(input("Enter a between 33 and 127: "))
+    while LOWER_VALUE > number_input & number_input > UPPER_VALUE:
+        number_input = int(input("Invalid number. Enter a between {} and {}: ".format(LOWER_VALUE, UPPER_VALUE)))
+    else:
+        print("The character for {} is {}".format(str(character_input), chr(character_input)))
+
+def get_number():
+
+
+main()
