6c9d427b8ea9d99abc85e177458b4618c56768fa
Hayden Hartley <hhart30@hotmail.com>
2017-04-20 11:19:52 +1000

    Update Prac06

---
+++ Prac06/strToLabel.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Label
+from kivy.properties import StringProperty
+
+
+class StringToLabel(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.string_list = ["word", "more words", "another word", "blah"]
+
+    def build(self):
+        self.title = "List of Strings"
+        self.root = Builder.load_file('strToLabel.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for string in self.string_list:
+            temp_label = Label(text=string)
+            self.root.ids.stringList.add_widget(temp_label)
+
+
+StringToLabel().run()
