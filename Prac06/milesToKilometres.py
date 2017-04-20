40014622ed1daa271a2ffaa2b6bda7c4bfaabe9b
Hayden Hartley <hhart30@hotmail.com>
2017-04-20 10:34:07 +1000

    Update Prac06

--- Prac06/milesToKilometres.py
+++ Prac06/milesToKilometres.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+MILES_TO_KILOMETRES = 1.60934
+
 
 class MilesToKilometres(App):
     def build(self):
@@ -10,8 +12,19 @@ class MilesToKilometres(App):
         self.root = Builder.load_file('milesToKilometres.kv')
         return self.root
 
-    def handle_calculate_km(self, miles):
-        kilometres = miles * 1.60934
+    def handle_calculate_km(self):
+        kilometres = self.get_valid_number() * MILES_TO_KILOMETRES
         self.root.ids.output_label.text = str(kilometres)
+
+    def handle_count(self, difference):
+        self.root.ids.miles_input.text = str(self.get_valid_number() + difference)
+
+    def get_valid_number(self):
+        try:
+            valid_num = float(self.root.ids.miles_input.text)
+            return valid_num
+        except ValueError:
+            return 0
+
 
 MilesToKilometres().run()
